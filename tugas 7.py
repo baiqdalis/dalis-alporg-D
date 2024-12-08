e7138372d073dac033169d7feaacf0216096a7b8
baiqdalis <baiqabdalis@gmail.com>
2024-12-08 17:34:07 +0800

    Add files via upload

---
+++ tugas 7.py
@@ -0,0 +1,20 @@
+# ARITMATIKA(X : + - ** % //)
+
+a = 24
+b = 27
+
+# Penjumlahan 
+hasil = a + b
+print("nilai dari a + b = ", hasil)
+
+# Perpangkatan 
+hasil = a ** b
+print("nilai dari a pangkat b = ", hasil)
+
+# Modulus (sisa bagi)
+hasil = a % b
+print("nilai dari a Mod b = ", hasil)
+
+#floor dividion (pembagian terdekat-kebawah)
+hasil = a // b
+print("nilai dari a flor b = ", hasil)
